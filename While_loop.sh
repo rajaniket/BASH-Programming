434bd60423c7c9920f84f2e42247ca953ecf943c
Aniket Raj <aniketraj.go@gmail.com>
2020-06-26 22:39:47 +0530

    Update While_loop.sh

--- While_loop.sh
+++ While_loop.sh
@@ -1,6 +1,14 @@
 x=10
-while [ $x -le 100 ]
+while [ $x -le 100 ]    # less then or equal 
 do
   echo "Welcome $x times"
   x=$(( $x + 10 ))
+done
+
+a=0
+
+while [ $a -lt 10 ]   # less than
+do
+   echo $a
+   a=`expr $a + 1`
 done
