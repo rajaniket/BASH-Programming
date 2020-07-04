e26426cb20bfbc2a256f61484ae1f0bfa8f38c43
Aniket Raj <aniketraj.go@gmail.com>
2020-07-04 23:59:40 +0530

    command line argument

--- command_argument.sh
+++ command_argument.sh
@@ -4,6 +4,6 @@ echo "File Name: $0"          # $0 will give the filename of the current script
 echo "First Parameter : $1"   # $n n is postive decimal number 
 echo "Second Parameter : $2"    # if input is Ani Raj 
                                 # then first parameter wiil be Ani and second will be Raj
-echo "Quoted Values: $@"
-echo "Quoted Values: $*"
-echo "Total Number of Parameters : $#"
+echo "Quoted Values: $@"    # $@ is equivalent to $1 $2 means it will take both Ani raj like string
+echo "Quoted Values: $*"  # All the arguments are double quoted. If a script receives two arguments, $* is equivalent to $1 $2.
+echo "Total Number of Parameters : $#"   # The number of arguments supplied to a script.   it supports only number
