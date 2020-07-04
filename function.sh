d567e2053d5ac9edff637739a98a0c2bd216a666
Aniket Raj <aniketraj.go@gmail.com>
2020-07-04 23:47:53 +0530

    function

--- function.sh
+++ function.sh
@@ -1,7 +1,21 @@
 # Define your function here
 Hello () {
-   echo "Hello Raj"
+   echo "Ani Raj"
 }
 
 # Invoke your function
-Hello
+Hello   # calling 
+
+# with argument
+Hello () {
+   echo "Hello World $1 $2"
+   return 10
+}
+
+# Invoke your function
+Hello ani raj   # Hello is function ani nd raj is input ti $1 and $2
+
+# Capture value returnd by last command
+ret=$?
+
+echo "Return value is $ret"
