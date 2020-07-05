06117438ebce2b2aaac483754321ecc4ac70f715
Aniket Raj <aniketraj.go@gmail.com>
2020-07-05 23:48:11 +0530

    Until loop

--- until_loop.sh
+++ until_loop.sh
@@ -1,7 +1,9 @@
 # Here, if the COMMANDS get evaluated to false then the statements will be executed. If the COMMANDS get evaluated to true then the no statements will be executed and control will go after the done statement.
+# The while loop is perfect for a situation where you need to execute a set of commands while some condition is true. Sometimes you need to execute a set of commands until a condition is true.
+
 i=1
 until [ $i -gt 6 ]
 do
 	echo "$i times."
-	i=$(( i+1 ))
+	i=$(( i+1 ))  # the (( ... )) construct permits arithmetic expansion and evaluation
 done
