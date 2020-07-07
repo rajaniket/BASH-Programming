d42e1a2f71e8cc96b0441492e442bc0cc609c4c8
Aniket Raj <aniketraj.go@gmail.com>
2020-07-07 23:46:16 +0530

    Concatenating Strings

--- Example_2.sh
+++ Example_2.sh
@@ -4,3 +4,8 @@ echo "$string1$string2"
 string3=$string1+$string2
 string3+=" is a good tutorial blog site"
 echo $string3
+ 
+string1="Ubuntu"
+string2="Pit"
+string=$string1$string2
+echo "$string is a great resource for Linux beginners."
