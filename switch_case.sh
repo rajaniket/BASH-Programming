e57c5919885c973a32c18671e88040e94d056152
Aniket Raj <aniketraj.go@gmail.com>
2020-07-10 23:36:09 +0530

    switch case

---
+++ switch_case.sh
@@ -0,0 +1,28 @@
+# case word in 
+#  pattern1)
+#    Statement(s) to be executed if pattern1 matches
+#   ;;
+# pattern2)
+#   Statement(s) to be executed if pattern2 matches
+#  ;;
+# pattern3)
+#   Statement(s) to be executed if pattern3 matches
+#  ;;
+#  *)
+#    Default condition to be executed
+#   ;;
+# esac
+ 
+CARS="audi"
+  
+#Pass the variable in string 
+case "$CARS" in 
+    #case 1 
+    "mercedes") echo "Headquarters - Affalterbach, Germany" ;; 
+      
+    #case 2 
+    "audi") echo "Headquarters - Ingolstadt, Germany" ;; 
+      
+    #case 3 
+    "bmw") echo "Headquarters - Chennai, Tamil Nadu, India" ;; 
+esac 
